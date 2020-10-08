5018e98dee0805b07fa8588b946e64a78c838a93
dmoore91 <danmoore2706@gmail.com>
2020-10-08 11:16:27 -0400

    Implement add permission

--- internal/app/permission/permission.go
+++ internal/app/permission/permission.go
@@ -1,12 +1,49 @@
 package permission
 
 import (
+	"database/sql"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"voting_web_service/internal/app/responses"
 )
 
 func AddPermission(writer http.ResponseWriter, request *http.Request) {
-	responses.GeneralNotImplemented(writer)
+	params := mux.Vars(request)
+
+	db, err := sql.Open("mysql", "root:root@tcp(0.0.0.0:3306)/test")
+	if err != nil {
+		responses.GeneralSystemFailure(writer, "Cannot connect to db")
+		log.Error(err)
+		return
+	}
+
+	defer db.Close()
+
+	queryString := "INSERT INTO Permissions(permission) " +
+		"VALUES(?)"
+
+	r, err := db.Exec(queryString, params["permission"])
+	if err != nil {
+		responses.GeneralSystemFailure(writer, "Query Failed")
+		log.Error(err)
+		return
+	}
+
+	rowsAffected, err := r.RowsAffected()
+
+	if err != nil {
+		responses.GeneralSystemFailure(writer, "Query Failed")
+		log.Error(err)
+		return
+	}
+
+	if rowsAffected == 0 {
+		responses.GeneralSystemFailure(writer, "Query Failed")
+		return
+	}
+
+	responses.GeneralSuccess(writer, "Success")
 }
 
 func GetUsersForPermission(writer http.ResponseWriter, request *http.Request) {
--- internal/app/permission/routes.go
+++ internal/app/permission/routes.go
@@ -3,6 +3,6 @@ package permission
 import "github.com/gorilla/mux"
 
 func InitializeRoutes(router *mux.Router, basePath string) {
-	router.HandleFunc(basePath+"/permission", AddPermission).Methods("POST")
+	router.HandleFunc(basePath+"/permission/{permission}", AddPermission).Methods("POST")
 	router.HandleFunc(basePath+"/permission/{permission}", GetUsersForPermission).Methods("GET")
 }
