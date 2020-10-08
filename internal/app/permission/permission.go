d5c39d0af7d5f6b9e9401d287c0293456cf50e1c
dmoore91 <danmoore2706@gmail.com>
2020-10-08 11:29:14 -0400

    Implement get users for permission

--- internal/app/permission/permission.go
+++ internal/app/permission/permission.go
@@ -2,11 +2,17 @@ package permission
 
 import (
 	"database/sql"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"voting_web_service/internal/app/responses"
+	"voting_web_service/internal/app/users"
 )
+
+type UsersStruct struct {
+	Users []users.User `json:"users"`
+}
 
 func AddPermission(writer http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
@@ -47,5 +53,50 @@ func AddPermission(writer http.ResponseWriter, request *http.Request) {
 }
 
 func GetUsersForPermission(writer http.ResponseWriter, request *http.Request) {
-	responses.GeneralNotImplemented(writer)
+	params := mux.Vars(request)
+
+	db, err := sql.Open("mysql", "root:root@tcp(0.0.0.0:3306)/test")
+	if err != nil {
+		responses.GeneralSystemFailure(writer, "Cannot connect to db")
+		log.Error(err)
+		return
+	}
+
+	defer db.Close()
+
+	queryString := "SELECT user_id, username, email, first_name, last_name, party_id " +
+		"FROM Users " +
+		"INNER JOIN User_Permissions ON User_Permissions.user_id = Users.user_id " +
+		"INNER JOIN Permissions ON Permissions.permission_id = User_Permissions.permission_id" +
+		"WHERE permission=?"
+
+	rows, err := db.Query(queryString, params["permission"])
+	if err != nil {
+		responses.GeneralSystemFailure(writer, "Failed query")
+		log.Error(err)
+		return
+	}
+
+	var userList []users.User
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var u = users.User{}
+		err = rows.Scan(&u.UserId, &u.Username, &u.Email, &u.FirstName, &u.LastName, &u.Party)
+
+		if err != nil {
+			responses.GeneralSystemFailure(writer, "Get Failed")
+			log.Error(err)
+			return
+		}
+
+		userList = append(userList, u)
+	}
+
+	resp := UsersStruct{Users: userList}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(200)
+	_ = json.NewEncoder(writer).Encode(resp)
 }
