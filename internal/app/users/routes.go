5aa29873625bc3faea20b150373018aca1b5230d
dmoore91 <danmoore2706@gmail.com>
2020-10-28 21:52:07 -0400

    Removed unecessary warning

--- internal/app/users/routes.go
+++ internal/app/users/routes.go
@@ -7,10 +7,6 @@ func InitializeRoutes(router *mux.Router, basePath string) {
 	router.HandleFunc(basePath+"/user/{username}", UpdateUser).Methods("PUT")
 	router.HandleFunc(basePath+"/user/{username}", GetUser).Methods("GET")
 	router.HandleFunc(basePath+"/user/login", LoginUser).Methods("POST")
-
-	// Order below is very important because if /user/permission/{username} isn't first it will never get called
-	// because the other requests will suck it up. Go matched these requests in a top down order and takes the first
-	// thing to match
 	router.HandleFunc(basePath+"/user/permission/{username}", GetPermissionsForUser).Methods("GET")
 	router.HandleFunc(basePath+"/user/{username}/{permission}", AddPermissionForUser).Methods("POST")
 	router.HandleFunc(basePath+"/user/{username}/{permission}", RemovePermissionForUser).Methods("DELETE")
