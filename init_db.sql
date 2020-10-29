16424a91b10205e67edebfbd68e308523f2ccb11
dmoore91 <danmoore2706@gmail.com>
2020-10-28 20:56:26 -0400

    Local changes

--- init_db.sql
+++ init_db.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS Users(
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     party_id INTEGER ,
+    session TEXT ,
     PRIMARY KEY(user_id),
     CONSTRAINT unique_username_user UNIQUE (username)
 );
---
+++ internal/app/common/session.go
@@ -0,0 +1,7 @@
+package common
+
+type sessionInfo struct {
+	SessionID string `json:"session_id"`
+	Email     string `json:"email"`
+	string    `json:"email"`
+}
