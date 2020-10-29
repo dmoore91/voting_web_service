3bd95aa0eef8badc566254dd44bdf994ad08905d
dmoore91 <danmoore2706@gmail.com>
2020-10-29 15:11:26 -0400

    Read TFA response into struct so it's cleaner

--- internal/app/tfa/tfa.go
+++ internal/app/tfa/tfa.go
@@ -1,16 +1,18 @@
 package tfa
 
 import (
+	"bytes"
 	"encoding/json"
-	"net/http"
-	"os/exec"
-	"os"
 	log "github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"os/exec"
 	"voting_web_service/internal/app/responses"
 )
 
-type TFAResponse struct {
-	TFA string `json:"TFA"`
+type TFA struct {
+	Secret string `json:"secret"`
+	Data   string `json:"data"`
 }
 
 func GetTfa(writer http.ResponseWriter, request *http.Request) {
@@ -18,12 +20,23 @@ func GetTfa(writer http.ResponseWriter, request *http.Request) {
 	out, err := exec.Command("/bin/bash", "internal/app/tfa/sample.sh").Output()
 	mydir, _ := os.Getwd()
 	if err != nil {
-		responses.GeneralSystemFailure(writer, "Failed to get 2FA secret and data " + mydir )
+		responses.GeneralSystemFailure(writer, "Failed to get 2FA secret and data "+mydir)
+		log.Error(err)
+		return
+	}
+
+	r := bytes.NewReader(out)
+
+	decoder := json.NewDecoder(r)
+	var t TFA
+	err = decoder.Decode(&t)
+	if err != nil {
+		responses.GeneralSystemFailure(writer, "Decode Failed")
 		log.Error(err)
 		return
 	}
 
 	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(200)
-	_ = json.NewEncoder(writer).Encode(&TFAResponse{TFA: string(out)})
+	_ = json.NewEncoder(writer).Encode(t)
 }
