8482d04a1708950f61e01f965a3de4952224b74c
dmoore91 <danmoore2706@gmail.com>
2020-10-29 20:19:00 -0400

    Wrote endpoint around validating 2fa

--- internal/app/tfa/tfa.go
+++ internal/app/tfa/tfa.go
@@ -2,17 +2,27 @@ package tfa
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 	"voting_web_service/internal/app/responses"
+	"voting_web_service/internal/app/session"
 )
 
 type TFA struct {
 	Secret string `json:"secret"`
 	Data   string `json:"data"`
+}
+
+// swagger:model validationInput
+type ValidateStruct struct {
+	SessionInfo session.SessionInfo `json:"session_info"`
+	Token       string              `json:"token"`
+	Username    string              `json:"username"`
 }
 
 func GetTfa(writer http.ResponseWriter, request *http.Request) {
@@ -41,8 +51,91 @@ func GetTfa(writer http.ResponseWriter, request *http.Request) {
 	_ = json.NewEncoder(writer).Encode(t)
 }
 
+func getSecretForUser(writer http.ResponseWriter, username string) (string, bool) {
+
+	db, err := sql.Open("mysql", "root:secret@tcp(0.0.0.0:3306)/voting")
+	if err != nil {
+		responses.GeneralSystemFailure(writer, "Cannot connect to db")
+		log.Error(err)
+		return "", false
+	}
+
+	defer db.Close()
+
+	queryString := "SELECT secret_key " +
+		"FROM Users " +
+		"WHERE username=?"
+
+	var secret string
+	err = db.QueryRow(queryString, username).Scan(&secret)
+	if err != nil {
+		responses.GeneralSystemFailure(writer, "Failed query")
+		log.Error(err)
+		return "", false
+	}
+
+	return secret, true
+}
+
 func Validate(writer http.ResponseWriter, request *http.Request) {
-	token := "GET IT FROM THE POST DATA that is sent through the frontend"
-	secret := "GET IT FROM THE USER TABLE"
-	out, err := exec.Command("/bin/bash", "internal/app/tfa/validate.sh " + secret + " " + token).Output()
+	// POST /tfa_validate
+	//
+	// Endpoint to validate 2 factor authentication
+	//
+	// ---
+	// produces:
+	// - application/json
+	//  parameters:
+	//	 - name: validation_input
+	//	   in: body
+	//	   description: input for validating 2fa
+	//	   schema:
+	//	     "$ref": "#/definitions/validationInput"
+	//	   required: true
+	// responses:
+	//   '200':
+	//     description: User is valid
+	//     schema:
+	//       "$ref": "#/definitions/candidateList"
+	//   '400':
+	//     description: bad request
+	//     schema:
+	//       "$ref": "#/definitions/generalResponse"
+	//   '500':
+	//     description: server errorValidateStruct
+	//     schema:
+	//       "$ref": "#/definitions/generalResponse"
+	decoder := json.NewDecoder(request.Body)
+	var v ValidateStruct
+	err := decoder.Decode(&v)
+	if err != nil {
+		responses.GeneralBadRequest(writer, "Decode Failed")
+		log.Error(err)
+		return
+	}
+
+	valid := session.CheckSessionID(v.SessionInfo.Username, v.SessionInfo.SessionID)
+
+	if valid {
+		secret, legit := getSecretForUser(writer, v.Username)
+		if legit {
+			out, err := exec.Command("/bin/bash", "internal/app/tfa/validate.sh "+secret+" "+v.Token).
+				Output()
+			if err != nil {
+				responses.GeneralBadRequest(writer, "Validation Script Failed")
+				log.Error(err)
+				return
+			}
+
+			if strings.EqualFold(string(out), "true") {
+				responses.GeneralSuccess(writer, "Success")
+			} else {
+				responses.GeneralBadRequest(writer, "Token invalid")
+			}
+		} else {
+			responses.GeneralSystemFailure(writer, "Failed to get user secret")
+		}
+	} else {
+		responses.GeneralBadRequest(writer, "Bad Session Token")
+	}
 }
