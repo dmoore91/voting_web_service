f45d5fa0eea900cee7de625518acb91561d61683
Alberto Serrano <ascalva@gmail.com>
2020-11-16 11:36:51 -0500

    Nginx working without ssl

--- populate_db.py
+++ populate_db.py
@@ -11,18 +11,30 @@ def populate_users(df, mysql_db) :
 
     mysql_db.commit()
 
+def populate_parties(df, mysql_db) :
+    cursor = mysql_db.cursor()
+
+    sql_cmd = "INSERT INTO Party(party_id, party) VALUES(%s, %s)"
+
+    for _, row in df.iterrows() :
+        cursor.execute(sql_cmd, row.values.tolist())
+
+    mysql_db.commit()
 
 def main() :
-    csv_file = "data/user_data.csv"
-    df       = pd.read_csv(csv_file)
+    user_file  = "data/user_data.csv"
+    party_file = "data/party_data.csv"
+    df_users   = pd.read_csv(user_file)
+    df_party   = pd.read_csv(party_file)
 
     mysql_db = m.connect(
-        host="localhost",
+        host="mysql_db",
         user="root",
         password="secret",
         database="voting"
     )
 
-    populate_users(df, mysql_db)
+    #populate_users(df, mysql_db)
+    populate_parties(df_party, mysql_db)
 
 main()
